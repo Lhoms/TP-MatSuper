70c7bd3a7d38a4c90e2c37af3a298e2901325424
lhoms <lucashoms@gmail.com>
2016-10-25 17:10:17 -0300

    deberia ser asi, no me anda wavread..

---
+++ codigo.m
@@ -0,0 +1,10 @@
+
+
+%se lee el archivo de audio y retorna sample(y) y frecuencia (fs)
+[y,fs]=wavread('Audio-TPMatSup');
+
+%aca se calcula la duracion
+t=linspace(0, length(y)/fs, length(y));
+
+%se grafica
+plot(t, y)
