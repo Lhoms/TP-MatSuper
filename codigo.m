962add69af333d262fb48c18306196670b8e4d8c
Ivo Ursino <ivo@MacBook-Air-de-Ivo.local>
2016-10-28 00:14:26 -0300

    Agrego .wav al nombre del archivo a leer con wavread()

--- codigo.m
+++ codigo.m
@@ -1,7 +1,7 @@
 
 
 %se lee el archivo de audio y retorna sample(y) y frecuencia (fs)
-[y,fs]=wavread('Audio-TPMatSup');
+[y,fs]=wavread('Audio-TPMatSup.wav');
 
 %aca se calcula la duracion
 t=linspace(0, length(y)/fs, length(y));
