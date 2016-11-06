ed232395199ea0a44e8b6278694477ca0a97d9f4
felipecalvo <felipecalvo@yahoo.com.ar>
2016-11-06 12:22:25 -0300

    intentando antitransformar...

--- codigo.m
+++ codigo.m
@@ -1,10 +1,18 @@
 
-
 %se lee el archivo de audio y retorna sample(y) y frecuencia (fs)
-[y,fs]=wavread('Audio-TPMatSup.wav');
+[Audio,fs]=wavread('Audio-TPMatSup.wav');
+
+%convierto de stereo a mono
+xn = (Audio(:,1)+Audio(:,2))/2;
 
 %aca se calcula la duracion
-t=linspace(0, length(y)/fs, length(y));
+t=linspace(0, length(Audio)/fs, length(Audio));
 
 %se grafica
-plot(t, y)
+%plot(t, xn);
+
+YZ = filter ([1 1.1],[1 0 -0.1] , xn)
+
+yn = iztrans(YZ)
+
+plot(yn);
