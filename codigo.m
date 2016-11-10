9d50404a3fc04ff49a5d0b1e0d220b055ca0948f
ivo <ursinoivo@gmail.com>
2016-11-10 18:15:17 -0300

    Agrego descripcion a los graficos

--- codigo.m
+++ codigo.m
@@ -2,8 +2,12 @@ archivoDeAudio = 'Audio-TPMatSup.wav';
 [datos,frecuencia] = audioread(archivoDeAudio);
 
 %Audio original
+
 subplot(2,1,1);
 plot(datos);
+title('Audio original en funcion del tiempo (x(t))');
+xlabel('x');
+ylabel('t');
 
 numeradorFuncionFiltro = [1 1.1]; % Numerador de la función filtro H(z) -> z + 1.1 
 denominadorFuncionFiltro = [1 0 -0.1]; % Denominador de la función filtro H(z) -> ze2 - 0.1
@@ -11,5 +15,9 @@ denominadorFuncionFiltro = [1 0 -0.1]; % Denominador de la función filtro H(z) 
 audioFiltrado = filter(numeradorFuncionFiltro,denominadorFuncionFiltro,datos);
 
 %Audio filtrado
+
 subplot(2,1,2);
 plot(audioFiltrado);
+title('Audio filtrado en funcion del tiempo (y(t))');
+xlabel('y');
+ylabel('t');
